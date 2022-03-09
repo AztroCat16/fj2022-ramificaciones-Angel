01e350234038bd871f4f85bb54df62a6019591b7
Angel Efrain Sanchez Villarreal <19030364@uadeo.mx>
2022-03-08 19:26:21 -0700

    MySQL: DDL created

---
+++ db.sql
@@ -0,0 +1,10 @@
+create database mydb;
+USE mydb;
+
+create table users(
+   id integer primary key auto_increment,
+   name varchar(20) not null unique,
+   password varchar(300) not null,
+   created_at datetime not null default CURRENT_TIMESTAMP,
+   update_at datatime not null
+)
